f759dce5ecb2b3ee411c7accb85c5e5bc20dc3ff
saifullahshah <saifullahshah818@gmail.com>
2025-09-01 21:34:16 +0500

    lib update

--- app/src/main/java/com/revoio/composeadsexample/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.revoio.composeadsexample
-
-object Constants {
-    const val HOME_NATIVE_AD_UNIT_ID = "ca-app-pub-3940256099942544/2247696110"
-    const val HOME_BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/9214589741"
-}
